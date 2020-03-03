ba5d62660aead0b8ba3ad9d7080f1c5dc200e69c
danielrol <drolfe@carnival.com>
2020-03-03 12:08:29 -0500

    Initial commit of Project Injection solution and projects

---
+++ ProxyInjector/ProxyInjector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace ProxyInjector
+{
+    public class ProxyInjector : IWebProxy
+    {
+        public ICredentials Credentials
+        {
+            get => new NetworkCredential("userName", "password");
+            set { }
+        }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return new Uri("<proxy URI goes here>");
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            return false;
+        }
+    }
+}
